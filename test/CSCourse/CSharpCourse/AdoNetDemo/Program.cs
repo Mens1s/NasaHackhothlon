51ae50d035af0fc088452e06c1673540ebb253b9
System Administrator <root@Ahmet-MacBook-Air-3.local>
2024-10-06 06:45:39 +0300

    Web design-service added

---
+++ test/CSCourse/CSharpCourse/AdoNetDemo/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Data;
+
+namespace AdoNetDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductDal productDal = new ProductDal();
+
+            List<Product> products = productDal.GetAll();
+            
+            products.ForEach(product =>
+            {
+                Console.Write($"{TruncateOrPad(product.Id.ToString(), 11)}\t");
+                Console.Write($"{TruncateOrPad(product.Name, 11)}\t");
+                Console.Write($"{TruncateOrPad(product.UnitPrice.ToString(), 11)}\t");
+                Console.Write($"{TruncateOrPad(product.StockAmount.ToString(), 11)}\t");
+                Console.WriteLine();
+            });
+            
+            // early to early times
+            DataTable table = productDal.GetAll2();
+            
+            foreach (DataColumn column in table.Columns)
+            {
+                Console.Write($"{TruncateOrPad(column.ColumnName, 11)}\t");
+            }
+            Console.WriteLine();
+
+            foreach (DataRow row in table.Rows)
+            {
+                foreach (var item in row.ItemArray)
+                {
+                    Console.Write($"{TruncateOrPad(item.ToString(), 11)}\t");
+                }
+                Console.WriteLine();
+            }
+            
+            // add data
+            Product newProduct = new Product
+            {
+                Name = "Toplama Bilgisayar",
+                StockAmount = 23,
+                UnitPrice = 5999
+            };
+
+            //productDal.Add(newProduct);
+            
+            // update
+            newProduct.Id = 4;
+            newProduct.UnitPrice = 4999;
+            productDal.Update(newProduct);
+            
+            // delete
+            productDal.Delete(newProduct.Id);
+        }
+        private static string TruncateOrPad(string value, int maxLength)
+        {
+            if (value.Length > maxLength)
+            {
+                return value.Substring(0, maxLength); // Truncate if too long
+            }
+            else
+            {
+                return value.PadRight(maxLength); // Pad if too short
+            }
+        }
+    }
+}
--- test/CSCourse/CSharpCourse/Constructors/obj/Debug/net6.0/Constructors.AssemblyInfo.cs
+++ test/CSCourse/CSharpCourse/Constructors/obj/Debug/net6.0/Constructors.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Constructors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15b27199785bc42be9925156743168e578cd0200")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47a38b4c76823a51e0fc4e630842bb63218c4969")]
 [assembly: System.Reflection.AssemblyProductAttribute("Constructors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Constructors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
